97c562451504342595241494fb7e7fdb3e922aa6
Luiso <lecc628@gmail.com>
2022-05-18 08:07:44 +0200

    Use view composition (view discovery)

--- ModuleA/ModuleAModule.cs
+++ ModuleA/ModuleAModule.cs
@@ -1,17 +1,22 @@
-﻿using Prism.Ioc;
+﻿using ModuleA.Views;
+using Prism.Ioc;
 using Prism.Modularity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Prism.Regions;
 
 namespace ModuleA
 {
     public class ModuleAModule : IModule
     {
+        private readonly IRegionManager _regionManager;
+
+        public ModuleAModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
         public void OnInitialized(IContainerProvider containerProvider)
         {
+            _regionManager.RegisterViewWithRegion<ViewA>("ContentRegion");
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
