e9427e7ac197d9b97b6aa58f7bd4db5cc199f728
Luiso <lecc628@gmail.com>
2022-05-20 08:36:34 +0100

    Use a ViewModelLocator

---
+++ ModuleA/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Mvvm;
+
+namespace ModuleA.ViewModels
+{
+    public class ViewAViewModel : BindableBase
+    {
+        private string _title;
+
+        public ViewAViewModel()
+        {
+            _title = "Hello from ViewAViewModel";
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+    }
+}
