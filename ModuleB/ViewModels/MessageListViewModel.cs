307ccb2aaee7785156ffca9d9f172cb3dc531361
Luiso <lecc628@gmail.com>
2022-05-23 06:56:30 +0200

    Unsubscribe from an event
    Uses the delegate method to unsubscribe from an event.

--- ModuleB/ViewModels/MessageListViewModel.cs
+++ ModuleB/ViewModels/MessageListViewModel.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.ObjectModel;
-
+using Prism.Commands;
 using Prism.Events;
 using Prism.Mvvm;
 
@@ -11,17 +11,44 @@ namespace ModuleB.ViewModels
     {
         public const string Word_Luis = "Luis";
 
+        private bool _isSubscribed = true;
+        private readonly MessageSentEvent _messageSentEvent;
+
         public MessageListViewModel(IEventAggregator eventAggregator)
         {
-            eventAggregator.GetEvent<MessageSentEvent>().Subscribe(OnMessageSentEvent, ThreadOption.PublisherThread, false, 
-                message => message.Contains(Word_Luis));
+            SubscribedCommand = new DelegateCommand(SubUnsubEvent, () => true)
+                .ObservesProperty(() => IsSubscribed);
+
+            _messageSentEvent = eventAggregator.GetEvent<MessageSentEvent>();
+            SubUnsubEvent();
         }
+
+        public bool IsSubscribed
+        {
+            get => _isSubscribed;
+            set => SetProperty(ref _isSubscribed, value);
+        }
+
+        public DelegateCommand SubscribedCommand { get; private set; }
 
         public ObservableCollection<string> Messages { get; } = new();
 
         private void OnMessageSentEvent(string message)
         {
             Messages.Add(message);
+        }
+
+        private void SubUnsubEvent()
+        {
+            if (IsSubscribed)
+            {
+                _messageSentEvent.Subscribe(OnMessageSentEvent, ThreadOption.PublisherThread, false,
+                    message => message.Contains(Word_Luis));
+            }
+            else
+            {
+                _messageSentEvent.Unsubscribe(OnMessageSentEvent);
+            }
         }
     }
 }
